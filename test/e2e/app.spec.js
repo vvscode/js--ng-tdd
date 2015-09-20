58b114de1d0167faed3766489a50d8c04cea1b99
Vasil Vanchuk <v.vanchuk@tut.by>
2015-09-21 00:28:58 +0300

    Add first e2e test

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,5 +1,12 @@
 describe('end to end adress tests', function() {
   it('should have contact', function(done) {
     browser.get('http://localhost:8080');
+    element.all(by.repeater('contact in contacts'))
+      .then(function(contacts) {
+        var first = contacts[0];
+        var text = first.getText();
+        expect(text).toEqual('Vasil V');
+        done();
+      });
   });
 });
