34849b739bd1c86b24b825bddfc9381ea15699e9
Vasil Vanchuk <v.vanchuk@tut.by>
2015-09-20 22:57:16 +0300

    Check if service make http request

--- test/main.spec.js
+++ test/main.spec.js
@@ -4,17 +4,26 @@ var expect = chai.expect;
 describe('The Address Book App', function() {
 
   describe('the contact service', function() {
-    var contactService;
-    
-    it('should have a property contacts, an array', function() {
+    var contactService, $httpBackend;
+
+    beforeEach(function() {
       module('AddressBook');
       inject(function($injector) {
         contactService = $injector.get('contactService');
+        $httpBackend = $injector.get('$httpBackend');
       });
-
-      expect(contactService.contacts).to.be.an('array');
-
-
     });
+
+    it('should have a property contacts, an array', function() {
+      expect(contactService.contacts).to.be.an('array');
+    });
+
+    it('should call the backend', function() {
+      $httpBackend
+        .expectGET('http://localhost:9001/contacts')
+        .respond(200, []);
+      $httpBackend.flush();
+    });
+
   });
 });
