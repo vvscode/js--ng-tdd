47ba309bfd6b062e3a73ef47c422dd4e9156c153
Vasil Vanchuk <v.vanchuk@tut.by>
2015-09-20 19:13:38 +0300

    Initial test

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,21 @@ gulp.task('serve', function() {
   gulp.watch(['app/**/*.*'])
     .on('change', browserSync.reload);
 });
+
+
+
+gulp.task('serve-test', function() {
+  browserSync.init({
+    notify: false,
+    port: 8081,
+    server: {
+      baseDir: ['test'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch(['test/**/*.*'])
+    .on('change', browserSync.reload);
+});
---
+++ test/main.spec.js
@@ -0,0 +1,5 @@
+describe('The Address Book App', function() {
+  it('Should work', function() {
+    chai.assert.isArray([]);
+  })
+});
